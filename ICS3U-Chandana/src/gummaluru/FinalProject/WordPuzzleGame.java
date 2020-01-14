cf92a3b5da70ff14d8026d66457a9c512e2028e7
333775336 <333775336@C-4028YR1.tdsb.on.ca>
2020-01-14 15:06:44 -0500

    Word puzzle game

--- ICS3U-Chandana/src/gummaluru/FinalProject/WordPuzzleGame.java
+++ ICS3U-Chandana/src/gummaluru/FinalProject/WordPuzzleGame.java
@@ -10,21 +10,28 @@ public class WordPuzzleGame {
 	public static void main(String[]args) {
 		Scanner sc = new Scanner(System.in);
 		
+		//variables for Player 1 and Player 2 score values.
 		int scorePlayer1=0;
 		int scorePlayer2=0;
 		
+		//answers for each puzzle.
 		String puzzle1 = "Captain Jack Sparrow";
 		String puzzle2 = "Prince Hans of the Seven Isles";
 		
-		for (int i=0; i<puzzle1.length(); i++) {	
+		//prints the empty spaces for the puzzle. 
+		for(int i=0; i<puzzle1.length(); i++) {	
+			
 			System.out.print("__ ");
 		}
 		
+		System.out.println("Enter your letter:");
+		sc.nextLine();
 		
 		
+	
 	}
 	
-	public static void scoreChange(int newScore1, int newScore2) {
+	public static  {
 		
 	}
 	
