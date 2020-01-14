72b7062a4962dfba0aead62b35436bda2fcbc2ae
333775336 <333775336@C-4028YR1.tdsb.on.ca>
2020-01-14 15:06:14 -0500

    Word puzzle game first draft

---
+++ ICS3U-Chandana/src/gummaluru/FinalProject/WordPuzzleGame.java
@@ -0,0 +1,31 @@
+package gummaluru.FinalProject;
+import java.util.Scanner;
+
+/**
+ * 
+ * Tues, Jan 14th, 2020
+ * @author Chandana Gummaluru
+ */
+public class WordPuzzleGame {
+	public static void main(String[]args) {
+		Scanner sc = new Scanner(System.in);
+		
+		int scorePlayer1=0;
+		int scorePlayer2=0;
+		
+		String puzzle1 = "Captain Jack Sparrow";
+		String puzzle2 = "Prince Hans of the Seven Isles";
+		
+		for (int i=0; i<puzzle1.length(); i++) {	
+			System.out.print("__ ");
+		}
+		
+		
+		
+	}
+	
+	public static void scoreChange(int newScore1, int newScore2) {
+		
+	}
+	
+}
