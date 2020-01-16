8b8568ba68f93002ce85d021cf783749834a6ba8
333775336 <333775336@C-4028YR1.tdsb.on.ca>
2020-01-16 15:05:29 -0500

    word puzzle game

--- ICS3U-Chandana/src/gummaluru/FinalProject/WordPuzzleGame.java
+++ ICS3U-Chandana/src/gummaluru/FinalProject/WordPuzzleGame.java
@@ -9,71 +9,153 @@ import java.util.Scanner;
 public class WordPuzzleGame {
 	public static void main(String[]args) {
 		Scanner sc = new Scanner(System.in);
-
-		//variables for Player 1 and Player 2 scores.
-		int score1 = 0;
-		int score2 = 0;
 		
-        
-		boolean firstTurn = true;
-		boolean guessed = false;
+		System.out.println("Welcome to Puzzling Words! Let's begin.");
+		System.out.println("Player 1 can start the game.");
+		System.out.println("Pick a letter, buy a vowel, or solve the puzzle.");
+		System.out.println("Remember:");
+		System.out.println("Buying a vowel costs you points.");
+		System.out.println("For example, if there are 3 a's in the puzzle, you will lose 3 points.");
+	
+		//array of 2 indexes that holds the values of both player's scores.
+		int totalScores[] = new int[2];
 
-		//answers for each puzzle.
-		String puzzle = "Captain Jack Sparrow";
+		//array of multiple puzzles.
+		String listOfPuzzles[] = new String[]{
+				"Running through the woods.", 
+				"Eating fish from the water.", 
+				"Climbing up the big birch tree."};
+
+		//variable that holds the number of puzzles in the game.
+		int puzzleNumber = 0;
+
+
+		while (puzzleNumber < 5) {
+			//select a random puzzle.
+			String puzzle = selectPuzzle(listOfPuzzles);
+
+			//play the puzzle.
+			int scores[] = play(puzzle);
+
+			//add scores.
+			totalScores[0] += scores[0];
+			totalScores[1] += scores[1];
+
+			//next puzzle.
+			puzzleNumber += 1;
+		}
+	}
+
+	/**
+	 * This method selects a random puzzle from the array of puzzles.
+	 * @param listOfPuzzles
+	 * @return
+	 */
+	public static String selectPuzzle(String[] listOfPuzzles) {
+		int randomPuzzle = (int)(Math.random()*2);
+		return listOfPuzzles[randomPuzzle];
+	}
+
+	/**
+	 * This methods allows for the  
+	 * @param puzzle
+	 * @return
+	 */
+	public static int[] play(String puzzle) {
+		boolean turn1 = true;
+		boolean guessed = false;
+		boolean[] letterGuessed = new boolean[26];
+
+		int scores[] = new int[2];
+
 		boolean[] charsToDisplay = new boolean[puzzle.length()];
+
 		int charsLeft = setupPuzzle(puzzle, charsToDisplay);
 		displayPuzzle(puzzle, charsToDisplay);
 
-	
+		Scanner sc = new Scanner(System.in);
 
+		while (!guessed && charsLeft > 0) {
+			char guess = sc.next().charAt(0);
+
+			if (letterGuessed[guess - 'a']){
+				continue;
+			}
+
+			letterGuessed[guess - 'a'] = true;
+
+			int count = guessLetter(guess, puzzle, charsToDisplay);
+			displayPuzzle(puzzle, charsToDisplay);
+
+			int multiply = 1;
+			if (isVowel(guess)) {
+				multiply = -1;
+			}
+
+			if (turn1) {
+				scores[0] += multiply * count;
+			} 
+			
+			else {
+				scores[1] += multiply * count;
+			}
+
+			turn1 = !turn1;
+
+			charsLeft -= count;
+			displayScore(scores[0], scores[1]);
+		}
+		return scores;
 	}
-    
+
 	/**
-	 * This method returns the player's scores.
+	 * This method displays the players' scores.
 	 * @param score1
 	 * @param score2
 	 */
 	public static void displayScore(int score1, int score2) {
-		System.out.println("Player 1: " + score1);
-		System.out.println("Player 2: " + score2);
+		System.out.println("Player 1: " + score1 + " points");
+		System.out.println("Player 2: " + score2 + " points");
 	}
+
 	/**
-	 * This method 
+	 * 
 	 * @param letter
 	 * @param puzzle
 	 * @param charsToDisplay
 	 * @return
 	 */
-	public static int guessLetter(char letter, String puzzle, boolean[]charsToDisplay) {
+	public static int guessLetter(char letter, String puzzle, boolean[] charsToDisplay) {
 		int count = 0;
-		
-		//checks through the entire puzzle to see if the guessed letter is within it.
-		for(int i =0; i<puzzle.length(); i++) {
-			if (puzzle.charAt(i)==letter) {
+
+		for (int i = 0; i < puzzle.length(); i++) {
+			if (puzzle.charAt(i) == letter) {
 				charsToDisplay[i] = true;
 				count++;
-			}	
+			}
 		}
-		return count;	
+
+		return count;
 	}
+
 	/**
-	 * 
+	 * This methods displays the puzzle.
 	 * @param puzzle
 	 * @param charsToDisplay
 	 */
-	public static void displayPuzzle(String puzzle, boolean[]charsToDisplay) {
+	public static void displayPuzzle(String puzzle, boolean[] charsToDisplay) {
+
 		String displayPuzzle = "";
 
-		for(int i = 0; i<puzzle.length(); i++) {
+		for (int i = 0; i < puzzle.length(); i++) {
 			if (charsToDisplay[i] == true) {
-				displayPuzzle = displayPuzzle + charAt(i);
+				displayPuzzle += puzzle.charAt(i);
+			} else {
+				displayPuzzle += " _ ";
 			}
-			else {
-				displayPuzzle = displayPuzzle+"__";
-			}
+		}
 
-			System.out.println(displayPuzzle);
-		}	
+		System.out.println(displayPuzzle);
 	}
 	/**
 	 * 
@@ -81,30 +163,41 @@ public class WordPuzzleGame {
 	 * @param charsToDisplay
 	 * @return
 	 */
-	public static int setupPuzzle(String puzzle, boolean[]charsToDisplay) {
-		int counter = 0;
-		for(int i = 0; i<puzzle.length(); i++) {
-			char display = puzzle.charAt(i);
-			if(!letter(display)) {
-				charsToDisplay[i]=true;
-				counter++;
+	public static int setupPuzzle(String puzzle, boolean[] charsToDisplay) {
+		int count = 0;
+
+		for (int i = 0; i < puzzle.length(); i++) {   
+			//get the ith character.
+			char curr = puzzle.charAt(i);
+
+			//if the ith character is not a letter...
+			if (!isLetter(curr)) {
+				//make sure that character is already displayed.
+				charsToDisplay[i] = true;
+				count++;
 			}
 		}
-		return puzzle.length()-counter;
-	}
-	/**
-	 * 
-	 * @param letter
-	 * @return
-	 */
-	public static boolean isVowel(char letter) {
-		if (letter == 'a' || letter == 'e' || letter == 'o' || letter == 'u' || letter == 'i' 
-			|| letter == 'A' || letter == 'E' || letter == 'O' || letter == 'U' || letter == 'I') {
-			isVowel = true;
-			
-		}
+
+		return puzzle.length() - count;
 	}
 
+	/**
+	 * 
+	 * @param c
+	 * @return the vowel in the puzzle.
+	 */
+	public static boolean isVowel(char c) {
+		return c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u' || c == 'A' || c == 'E' || c == 'I' || c == 'O' || c == 'U';
+	}
+
+	/**
+	 * 
+	 * @param c
+	 * @return any letter in the puzzle.
+	 */
+	public static boolean isLetter(char c) {
+		return (c >= 'A' && c <= 'Z') || (c >= 'a' && c <= 'z');
+	}
 
 
 }
